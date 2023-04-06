4626b6e15c00d5267f45cfd5817bb261659616f5
Sarawoot Chaichana <sarawoot_202@hotmail.com>
2023-04-06 22:00:38 +0700

    Add files via upload

---
+++ examples/motor/motor.ino
@@ -0,0 +1,23 @@
+#include "easy3in1.h"
+
+void setup() {
+  Easy3in1();
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  motor(1,50);
+  motor(2,50);
+  motor(3,50);
+  motor(4,50);
+  delay(2000);
+  motor(1,-50);
+  motor(2,-50);
+  motor(3,-50);
+  motor(4,-50);
+  delay(2000);
+  motorStopAll();
+  delay(1000);
+}
